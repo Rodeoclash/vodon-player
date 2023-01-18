01a6c0c217322850c230cfd690a2d7a8d6588dbe
Sam Richardson <sam@richardson.co.nz>
2023-01-18 14:28:24 +1100

    Build for esnext

--- player/vite.config.ts
+++ player/vite.config.ts
@@ -5,6 +5,9 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    target: "esnext",
+  },
   plugins: [
     VitePWA({
       registerType: "autoUpdate",
