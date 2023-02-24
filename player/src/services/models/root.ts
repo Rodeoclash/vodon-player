1955dde70fc7523fe55b66986fc4407c9e966f96
Sam Richardson <sam@richardson.co.nz>
2023-02-24 16:41:51 +1100

    Fix for same length names

--- player/src/services/models/root.ts
+++ player/src/services/models/root.ts
@@ -116,9 +116,12 @@ export class RootStore extends Model({
       if (a.name < b.name) {
         return -1;
       }
+
       if (a.name > b.name) {
         return 1;
       }
+
+      return 0;
     });
   }
 }
