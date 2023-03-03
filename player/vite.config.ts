199b2949cb05d79fa21c8088ce3f33285d0c9d1d
Sam Richardson <sam@richardson.co.nz>
2023-03-03 21:30:53 +1100

    Remove PWA, handle sub routes

--- player/vite.config.ts
+++ player/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,12 +8,6 @@ export default defineConfig({
     target: "esnext",
   },
   plugins: [
-    VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
-      },
-    }),
     react({
       babel: {
         plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
