7ca396702a0cf30863980fafbc2ae1db3f49081f
Sam Richardson <sam@richardson.co.nz>
2023-03-10 16:34:40 +1100

    Improve header layout

--- player/src/components/ui/Modal.tsx
+++ player/src/components/ui/Modal.tsx
@@ -3,7 +3,7 @@ import BaseModal, { Props } from "react-modal";
 const Modal = ({ children, ...props }: Props) => {
   return (
     <BaseModal
-      overlayClassName="fixed inset-0 bg-black/70 flex items-center justify-center z-20"
+      overlayClassName="fixed inset-0 bg-black/70 flex items-center justify-center z-50"
       className="bg-zinc-800 z-30 relative min-w-[40rem]"
       {...props}
     >
--- player/src/layouts/Session.tsx
+++ player/src/layouts/Session.tsx
@@ -39,41 +39,50 @@ export default function Session() {
           <div className="flex items-center justify-center signika text-2xl border-r border-stone-700 px-4">
             VODON
           </div>
-          <nav className="flex items-stretch">
-            <NavLink className="navlink" to="/">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+          <nav className="flex items-stretch flex-grow">
+            <div className="flex items-stretch flex-grow">
+              <NavLink className="navlink" to="/">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+                  />
+                </svg>
+              </NavLink>
+
+              <NavLink className="navlink" to="review">
+                Review
+              </NavLink>
+
+              <NavLink className="navlink" to="setup">
+                Setup
+              </NavLink>
+            </div>
+
+            <div className="flex items-center flex-shrink gap-4 pr-4">
+              <button
+                className="btn btn-primary"
+                onClick={() => handleClickHelp()}
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
-                />
-              </svg>
-            </NavLink>
+                Help
+              </button>
 
-            <NavLink className="navlink" to="review">
-              Review
-            </NavLink>
-
-            <NavLink className="navlink" to="setup">
-              Setup
-            </NavLink>
-
-            <button className="navlink" onClick={() => handleClickHelp()}>
-              Help
-            </button>
-
-            <button className="navlink" onClick={() => handleClickExport()}>
-              Export session
-            </button>
+              <button
+                className="btn btn-primary"
+                onClick={() => handleClickExport()}
+              >
+                Export session
+              </button>
+            </div>
           </nav>
-          <div className="basis-5/12"></div>
         </header>
         <main className="flex-grow overflow-y-auto overflow-x-hidden">
           <Outlet />
