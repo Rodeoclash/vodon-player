8868019c8dd8d2f8e35b986bb4263b3ae9377361
Sam Richardson <sam@richardson.co.nz>
2023-03-15 20:54:02 +1100

    Document file handles storage

--- player/src/services/file_handles.ts
+++ player/src/services/file_handles.ts
@@ -6,7 +6,19 @@ export interface FileHandle {
 }
 
 class Storage extends Dexie {
+  // File handles to videos that have been copied into the OPFS. Copying
+  // allows Vodon to manage the file / not need to re-request read permissions
+  // but obviously impacts storage size.
+  //
+  // Intended that files downloaded from cloud storage will end up in this store.
   videoFileHandles!: Table<FileHandle, number>;
+
+  // File handles that are "local" to the users file system. It's highly likely
+  // that these will need to have read premissions re-requested on them after
+  // loading the app.
+  videoFileHandlesLocal!: Table<FileHandle, number>;
+
+  // References to frame grabs on bookmarks. Stored in the OPFS.
   videoFrameFileHandles!: Table<FileHandle, number>;
 
   constructor() {
